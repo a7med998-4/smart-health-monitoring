4455f4ba54319da507673d6819afd0ac74c3edb2
a7med998-4 <21s20951@mec.edu.om>
2025-12-31 00:37:38 +0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,7 @@ Input N          // number of users
 FOR i = 1 TO N + 1
     Input BloodPressure
     Input SugarLevel
-    IF (BloodPressure > 140 OR SugarLevel > 180)
+    IF (BloodPressure >= 140 OR SugarLevel > 180)
         Risk = "HIGH RISK"
     ELSE IF ( (BloodPressure >= 120 AND BloodPressure <= 140)
                OR (SugarLevel >= 140 AND SugarLevel <= 180) )
