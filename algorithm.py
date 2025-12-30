8e152a4f4f3040419515deaf4c0ebcaf970f2c1e
a7med998-4 <21s20951@mec.edu.om>
2025-12-31 00:31:40 +0400

    Create algorithm.py
    Algorithm uploaded to main branch

---
+++ algorithm.py
@@ -0,0 +1,16 @@
+START
+Input N          // number of users
+FOR i = 1 TO N
+    Input BloodPressure
+    Input SugarLevel
+    IF (BloodPressure > 140 OR SugarLevel > 180)
+        Risk = "HIGH RISK"
+    ELSE IF ( (BloodPressure >= 120 AND BloodPressure <= 140)
+               OR (SugarLevel >= 140 AND SugarLevel <= 180) )
+        Risk = "MEDIUM RISK"
+    ELSE
+        Risk = "LOW RISK"
+    ENDIF
+    Display Risk
+END FOR
+STOP
